ca637a7280aca587580118a8f53e7e17e9c6f0e6
jez <jeremy@jezweb.com>
2023-11-12 11:01:30 +0800

    update

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,6 +0,0 @@
-## Pull request checklist
-
-Please check if your PR fulfills the following requirements:
-
-- [ ] The changes have been tested successfully.
-- [ ] A changeset has been created (`npm run changeset`).
--- packages/authentication/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/authentication/classes/LinkWithPhoneNumberOptions.java
+++
@@ -1,8 +0,0 @@
-package io.capawesome.capacitorjs.plugins.firebase.authentication.classes;
-
-public class LinkWithPhoneNumberOptions extends SignInWithPhoneNumberOptions {
-
-    public LinkWithPhoneNumberOptions(String phoneNumber, boolean resendCode) {
-        super(false, phoneNumber, resendCode);
-    }
-}
